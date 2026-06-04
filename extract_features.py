5a717bc1656a1444a62e903a999ee380cefa5795
Elena <potapovalena62@gmail.com>
2026-06-04 18:20:18 +0300

    added CelebA dataset feature extraction logic

--- extract_features.py
+++ extract_features.py
@@ -5,6 +5,14 @@ from PIL import Image #для работы с картинками
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms #для нейронки
 from facenet_pytorch import InceptionResnetV1 #модели для обнаружения лиц
-# Настройка устройства для вычислений
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Используем устройство: {device}")
+
+class CelebaDataset(Dataset):
+    def __init__(self, image_dir):
+        self.image_dir = image_dir
+        self.image_names = sorted([x for x in os.listdir(image_dir) if x.endswith(('.jpg', '.png', '.jpeg'))])
+        self.transform = transforms.Compose([
+            transforms.Resize((160, 160)), #приводит картинку к размеру 160 на 160
+            transforms.ToTensor(), #из пикселей в тензор PyTorch
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])#центрирование данных
+        ])
+
